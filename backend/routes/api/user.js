b4e13ac2b98f4b0ead72f809eaa1dbbf9bfd303c
Vincent <tuanle.eagle@gmail.com>
2022-12-07 11:43:28 +0700

    Multi Method

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,14 +1,42 @@
-const express = require('express')
-const router = express.Router()
-const listData = require('../../data/userInfo')
+const express = require("express");
+const router = express.Router();
+const listData = require("../../data/userInfo");
+const _ = require('lodash');
 
-router.get('/', (req, res) => {
-  res.json(listData)
-})
 
-router.get('/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  res.json(listData.find(user => user.id === id))
-})
+router.get("/", (req, res) => {
+  res.json(listData);
+});
+
+router.route("/:id")
+  .get((req, res) => {
+    const id = parseInt(req.params.id);
+    res.json(listData.find((user) => user.id === id));
+  })
+  .post((req, res) => {
+    const id = parseInt(req.params.id);
+    const body = _.get(req, "body");
+
+    const idx = listData.findIndex(user => user.id === id);
+    if(idx !== -1) {
+      listData.splice(idx, 1, {
+        ...listData[idx],
+        name: body.name,
+        age: body.age,
+      })
+    }
+
+    res.json(listData[idx]);
+  })
+  .delete((req, res) => {
+    const id = parseInt(req.params.id);
+    const idx = listData.findIndex(user => user.id === id);
+    if(idx !== -1) {
+      listData.splice(idx, 1)
+      res.json("Delete success")
+    }else {
+      res.json("Id Not Found")
+    }
+  })
 
 module.exports = router;
